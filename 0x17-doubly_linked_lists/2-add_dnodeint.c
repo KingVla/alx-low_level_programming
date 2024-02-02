571c8a9b280037caf73b7c538e3c55d39aabeaf1
KingVla <scientiatenkorang5@gmail.com>
2024-02-02 10:45:45 -0800

    ok

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,33 +8,42 @@
  * @head: head of the list
  * @n: value of the element
  * Return: the address of the new element
- */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-{
-	dlistint_t *new;
-	dlistint_t *h;
+ *
+ **/
+#include <stdlib.h>
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
+/* Definition of a doubly linked list node */
+typedef struct dlistint_s {
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
 
-	new->n = n;
-	new->prev = NULL;
-	h = *head;
+/* Function to add a new node at the beginning of a dlistint_t list */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n) {
+    /* Allocate memory for the new node */
+    dlistint_t *new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL) {
+        return NULL; /* Return NULL if memory allocation failed */
+    }
 
-	if (h != NULL)
-	{
-		while (h->prev != NULL)
-			h = h->prev;
-	}
+    /* Initialize the new node */
+    new_node->n = n;
+    new_node->prev = NULL;
 
-	new->next = h;
+    /* If the list is empty, set the new node as the head */
+    if (*head == NULL) {
+        new_node->next = NULL;
+    } else {
+        /* Otherwise, set the next of the new node to the current head */
+        new_node->next = *head;
+        /* Update the previous of the current head to point to the new node */
+        (*head)->prev = new_node;
+    }
 
-	if (h != NULL)
-		h->prev = new;
+    /* Update the head to point to the new node */
+    *head = new_node;
 
-	*head = new;
-
-	return (new);
+    return new_node; /* Return the address of the new element */
 }
 
